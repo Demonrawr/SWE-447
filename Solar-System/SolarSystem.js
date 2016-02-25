c3d7f15cd2d4b0f00e0fdaac5b2cc6c3e9dc343c
Demonrawr <dulvinnguyen@gmail.com>
2016-02-25 13:52:40 -0800

    Update SolarSystem.js

--- Solar-System/SolarSystem.js
+++ Solar-System/SolarSystem.js
@@ -30,28 +30,28 @@ var SolarSystem = {
     color : [ 1.0, 0.0, 0.0, 1.0 ]
   },
   Jupiter : {
-    radius : 5,
-    distance : 2.5,
+    radius : 4,
+    distance : 2.3,
     color : [ 1.0, 153/255, 0.0, 1.0 ]
   },
   Saturn : {
-    radius : 4,
-    distance : 3.4,
+    radius : 3,
+    distance : 2.0,
     color : [ 1.0, 1.0, 0.0, 1.0 ]
   },
   Uranus : {
-    radius : 2.01,
-    distance : 4.0,
+    radius : 1.01,
+    distance : 1.8,
     color : [ 0.0, 0.1, 1.0, 1.0 ]
   },
   Neptune : {
     radius : 1.88,
-    distance : 4.6,
+    distance : 2.6,
     color : [ 0.0, 0.0, 1.0, 1.0 ]
   },
   Pluto : {
     radius : 0.186,
-    distance : 5.2,
+    distance : 2.9,
     color : [ 150/255, 75/255, 0.0, 1.0 ]
   }
 };
